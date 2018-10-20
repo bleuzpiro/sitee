1c827dd1bd7a6023d94d9d2d6006c3fecc2a6a9f
bleuzpiro <38590875+bleuzpiro@users.noreply.github.com>
2018-10-20 17:33:19 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Melanie Castro Sophrologue</title> <!--nom de la page-->
+<head>
+<?php include('header/header.php'); ?>
+</head>
+<style type="text/css">
+<?php include('style/style.css'); ?> <!--style.css est le fichier de mise en page-->
+</style>
+
+
+
+<body>
+
+
+</body>
+<footer>
+<?php include('footer/footer.php'); ?> <!--footer.php est le bas de la page -->
+</footer>
+</html>
